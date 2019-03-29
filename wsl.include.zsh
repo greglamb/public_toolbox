6278e4971e94efa16418f51bb1c00c86ac2194de
Greg Lamb <glamb@launchcg.com>
2019-03-29 11:59:44 -0700

    fix docker

--- wsl.include.zsh
+++ wsl.include.zsh
@@ -4,7 +4,7 @@ export WINHOME=/c/Users/$(~/cmd.exe /c 'echo %USERNAME%' | sed -e 's/\r//g')
 alias cmd="$WINHOME/bin/cmd.exe"
 alias code="cmd /c code"
 
-#plugins+=(zsh-nvm)
+export DOCKER_HOST=localhost:2375
 
 source ~/dotfiles/public_toolbox/base.include.zsh
 
