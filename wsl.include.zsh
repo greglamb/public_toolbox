3dfea64487cc88085a5be5e35989531facfce02a
Greg Lamb <glamb@launchcg.com>
2019-03-29 12:02:15 -0700

    better home method

--- wsl.include.zsh
+++ wsl.include.zsh
@@ -3,9 +3,10 @@ export WINHOME=/c/Users/$(~/cmd.exe /c 'echo %USERNAME%' | sed -e 's/\r//g')
 
 alias cmd="$WINHOME/bin/cmd.exe"
 alias code="cmd /c code"
+alias winhome="cd $WINHOME"
 
 export DOCKER_HOST=localhost:2375
 
 source ~/dotfiles/public_toolbox/base.include.zsh
 
-cd $WINHOME
+winhome
